3c8672318908a737eed5fb9b56f6123744a0e55b
JohnEdwinson <jasondlsrys@gmail.com>
2024-03-20 11:15:54 +0800

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+
+})
+export class HomePage {
+
+
+
+constructor() {
+ 
+  
+ 
+}
+btnClick(){
+console.log ("click")
+
+
+}
+
+}
